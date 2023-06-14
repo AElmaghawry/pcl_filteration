021fae144a691483f21346dec21d50a2f1f84bfc
Aelmaghawry <elmaghawri.ay@gmail.com>
2023-06-14 11:14:07 +0400

    Add the looping method to loop among data set

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ public:
                 cloud_cluster->width = indices.indices.size();
                 cloud_cluster->height = 1;
                 cloud_cluster->is_dense = true;
-                
+
                 clusters_vector.push_back(cloud_cluster);
 
                 std::stringstream ss;
@@ -123,125 +123,130 @@ public:
 
 int main(int argc, char **argv)
 {
-    std::string file_name = "../data/data_set/4/4.pcd";
-    
-    PointCloudProcessing pcProcessing;
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
-    if (!pcProcessing.loadPointCloud(file_name, cloud))
-        return -1;
+    std::string direcoty_name = "../data/data_set/";
+    // std::string file_name = "../data/data_set/4/4.pcd";
+    std::cout << "Enter the number of the data set " << std::endl;
+    int userSelection{0};
+    std::cin >> userSelection;
 
-    RegionGrowingSegmentation regionSegmentation;
-    regionSegmentation.performSegmentation(cloud);
-
-    const std::vector<pcl::PointIndices> &clusters = regionSegmentation.getClusters();
-    const pcl::PointCloud<pcl::PointXYZRGB>::Ptr &colored_cloud = regionSegmentation.getColoredCloud();
-    std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clusters_vector;
-    pcProcessing.saveClusterPointClouds(clusters, colored_cloud, 1500, clusters_vector);
-
-    pcProcessing.visualizePointCloud(colored_cloud, regionSegmentation.getNormals());
-
-    std::cout << clusters.size() << std::endl;
-
-    std::string cloud_of_intreset_dir = "../data/cloud_cluster_"; ///home/jeo/ku/pcl_filteration/data/cloud_cluster_0.pcd
-
-    std::vector<std::string> files_paths{};
-    PointCloudProcessing pcSelection;
-    
-    for (int i{0}; i < clusters.size(); i++)
+    for (int counter{1}; counter <= userSelection; counter++)
     {
-        files_paths.push_back(cloud_of_intreset_dir + std::to_string(i) + ".pcd");
-    }
-    std::cout << files_paths[1] << std::endl;
+        std::string file_name = direcoty_name + std::to_string(counter) + "/" + std::to_string(counter) + ".pcd";
 
-    std::list<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> pointClouds; 
-    int i {0} ; 
-    for (const std::string &filepath : files_paths)
-    {   
-        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-        pcl::io::loadPCDFile<pcl::PointXYZRGB>(filepath, *cloud);
-        pointClouds.push_back(cloud);
-        i++ ;
-        std::cout<< "Loading the pcd files ......" << i << std::endl ; 
+        PointCloudProcessing pcProcessing;
+        pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+        if (!pcProcessing.loadPointCloud(file_name, cloud))
+            return -1;
 
-    }
+        RegionGrowingSegmentation regionSegmentation;
+        regionSegmentation.performSegmentation(cloud);
 
-    size_t smallestSize = std::numeric_limits<size_t>::max();
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr smallestCloud;
+        const std::vector<pcl::PointIndices> &clusters = regionSegmentation.getClusters();
+        const pcl::PointCloud<pcl::PointXYZRGB>::Ptr &colored_cloud = regionSegmentation.getColoredCloud();
+        std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> clusters_vector;
+        pcProcessing.saveClusterPointClouds(clusters, colored_cloud, 1500, clusters_vector);
 
-    for (const auto& cloud : clusters_vector)
-    {
-        size_t cloudSize = cloud->size();
-        if (cloudSize < smallestSize)
+        pcProcessing.visualizePointCloud(colored_cloud, regionSegmentation.getNormals());
+
+        std::cout << clusters.size() << std::endl;
+
+        std::string cloud_of_intreset_dir = "../data/cloud_cluster_"; /// home/jeo/ku/pcl_filteration/data/cloud_cluster_0.pcd
+
+        std::vector<std::string> files_paths{};
+        PointCloudProcessing pcSelection;
+
+        for (int i{0}; i < clusters.size(); i++)
         {
-            smallestSize = cloudSize;
-            smallestCloud = cloud;
+            files_paths.push_back(cloud_of_intreset_dir + std::to_string(i) + ".pcd");
         }
+        std::cout << files_paths[1] << std::endl;
+
+        std::list<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> pointClouds;
+        int i{0};
+        for (const std::string &filepath : files_paths)
+        {
+            pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+            pcl::io::loadPCDFile<pcl::PointXYZRGB>(filepath, *cloud);
+            pointClouds.push_back(cloud);
+            i++;
+            std::cout << "Loading the pcd files ......" << i << std::endl;
+        }
+
+        size_t smallestSize = std::numeric_limits<size_t>::max();
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr smallestCloud;
+
+        for (const auto &cloud : clusters_vector)
+        {
+            size_t cloudSize = cloud->size();
+            if (cloudSize < smallestSize)
+            {
+                smallestSize = cloudSize;
+                smallestCloud = cloud;
+            }
+        }
+
+        std::cout << "The smalles pcl size is equal " << smallestCloud->width * smallestCloud->height << std::endl;
+
+        pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+        pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+
+        pcl::PointXYZRGB centroid;
+        pcl::computeCentroid(*smallestCloud, centroid);
+
+        for (pcl::PointXYZRGB &point : smallestCloud->points)
+        {
+            point.x -= centroid.x;
+            point.y -= centroid.y;
+            point.z -= centroid.z;
+        }
+        pcl::SACSegmentation<pcl::PointXYZRGB> seg;
+        seg.setOptimizeCoefficients(true);
+        seg.setModelType(pcl::SACMODEL_PLANE);
+        seg.setMethodType(pcl::SAC_RANSAC);
+        seg.setDistanceThreshold(0.0001);
+
+        seg.setInputCloud(smallestCloud);
+        seg.segment(*inliers, *coefficients);
+
+        if (inliers->indices.size() == 0)
+        {
+            PCL_ERROR("Could not estimate a planar model for the given dataset.");
+            return (-1);
+        }
+
+        std::cerr << "Model coefficients: " << coefficients->values[0] << " "
+                  << coefficients->values[1] << " "
+                  << coefficients->values[2] << " "
+                  << coefficients->values[3] << std::endl;
+
+        pcl::ModelCoefficients::Ptr coefficientsRef(new pcl::ModelCoefficients);
+        coefficientsRef->values.push_back(0.0);
+        coefficientsRef->values.push_back(0.0);
+        coefficientsRef->values.push_back(1.0);
+        coefficientsRef->values.push_back(0.0);
+
+        // pcl::visualization::PCLVisualizer viewer("Point Cloud Visualization");
+
+        // viewer.setBackgroundColor(0.0, 0.0, 0.0);
+        // viewer.addPointCloud<pcl::PointXYZRGB>(smallestCloud, "cloud");
+        // viewer.addPointCloud(smallestCloud, "point_cloud");
+
+        // viewer.addPlane(*coefficients, "plane");
+        // viewer.addPlane(*coefficientsRef, "refrence");
+
+        // viewer.setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 1.0, 1.0, 0.0, "plane");
+        // viewer.setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR, 0.0, 1.0, 1.0, "refrence");
+
+        std::cout << "________________________________________" << std::endl;
+
+        std::cout << "Measured Angle between refrence frame and Surface is = "
+                  << calcAngelTwoVectors(coefficientsRef, coefficients)
+                  << " degrees"
+                  << std::endl;
+
+        // viewer.addCoordinateSystem(0.1);
+        // viewer.spin();
     }
-
-    std::cout << "The smalles pcl size is equal " << smallestCloud->width * smallestCloud->height <<std::endl;
-
-    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-
-    pcl::PointXYZRGB centroid;
-    pcl::computeCentroid(*smallestCloud, centroid);
-
-    for (pcl::PointXYZRGB& point : smallestCloud->points)
-    {
-        point.x -= centroid.x;
-        point.y -= centroid.y;
-        point.z -= centroid.z;
-    }
-    pcl::SACSegmentation<pcl::PointXYZRGB> seg;
-    seg.setOptimizeCoefficients (true);
-    seg.setModelType (pcl::SACMODEL_PLANE);
-    seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setDistanceThreshold (0.0001);
-
-    seg.setInputCloud (smallestCloud);
-    seg.segment (*inliers, *coefficients);
-    
-    if (inliers->indices.size () == 0)
-    {
-        PCL_ERROR ("Could not estimate a planar model for the given dataset.");
-        return (-1);
-    }
-
-    std::cerr << "Model coefficients: " << coefficients->values[0] << " " 
-                                        << coefficients->values[1] << " "
-                                        << coefficients->values[2] << " " 
-                                        << coefficients->values[3] << std::endl;
-                                        
-
-    pcl::ModelCoefficients::Ptr coefficientsRef (new pcl::ModelCoefficients);
-    coefficientsRef->values.push_back(0.0); 
-    coefficientsRef->values.push_back(0.0); 
-    coefficientsRef->values.push_back(1.0); 
-    coefficientsRef->values.push_back(0.0);   
-
-
-    pcl::visualization::PCLVisualizer viewer("Point Cloud Visualization");
-    
-    viewer.setBackgroundColor(0.0, 0.0, 0.0);
-    viewer.addPointCloud<pcl::PointXYZRGB>(smallestCloud, "cloud");
-    viewer.addPointCloud(smallestCloud, "point_cloud");
-    
-    viewer.addPlane(*coefficients,"plane");
-    viewer.addPlane(*coefficientsRef,"refrence"); 
-
-    viewer.setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR,1.0,1.0,0.0,"plane"); 
-    viewer.setShapeRenderingProperties(pcl::visualization::PCL_VISUALIZER_COLOR,0.0,1.0,1.0,"refrence"); 
-
-    std::cout << "________________________________________"<<std::endl ; 
-
-    std::cout   << "Measured Angle between refrence frame and Surface is = "
-                << calcAngelTwoVectors(coefficientsRef,coefficients)
-                << " degrees"
-                <<std::endl ; 
-                
-    viewer.addCoordinateSystem(0.1);
-    viewer.spin();
 
     return 0;
-
 }
